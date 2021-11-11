19d5cf0aa8fa6ba9a9bf7e7cc3f7dcf82f5ae2ca
shinhojung814 <shinhojung814@gmail.com>
2021-11-12 01:35:20 +0900

    project-alien

---
+++ project-alien/client/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import MultiAquarium from "./pages/MultiAquarium";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MultiAquarium />
+    </div>
+  );
+}
+
+export default App;
---
+++ project-alien/client/src/pages/MultiAquarium/index.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect, useRef } from "react";
+
+import Room from "./Room";
+import Header from "./Header";
+import FieldCtrl from "./FieldCtrl";
+import MultiField from "./MultiField";
+import * as api from "../../apis";
+import * as socket from "../../apis/socket";
+import background from "./image/univ.jpg"
+import styles from "./index.module.css";
+
+export default function MultiAquarium() {
+  const [testNum, setTestNum] = useState(-1);
+
+
+  const [roomIds, setRoomIds] = useState([]);
+  const [currRoomId, setCurrRoomId] = useState(null);
+  const rooms = useRef();
+
+  // 챌린지 정보 가져오기
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const res = await api.get("/test");
+        const data = await res.json();
+        console.log(data);
+        setTestNum(Math.round(data.body * 10000) / 10000);
+
+        // 서버에서 데이터를 받아온 상황을 전제로 구성
+        let roomIds = [1, 2, 3];
+
+        // rooms 상태 정보
+        rooms.current = {};
+        roomIds.forEach((roomId) => {
+          rooms.current[roomId] = new Room(roomId);
+        });
+
+        // roomIds: react에서 state로 관리할 정보
+        setRoomIds(roomIds);
+        setCurrRoomId(roomIds[0]);
+        console.log("fetchData", rooms.current);
+      };
+      fetchData();
+    } catch (err) {
+      console.error(err);
+    }
+  }, []);
+
+  useEffect(() => {
+    // rooms가 생성되었는지 확인
+    if (!rooms.current) return;
+
+    console.log("set currRoomId", currRoomId);
+    socket.initAndJoin(currRoomId);
+    socket.subscribe(rooms.current[currRoomId].updateFieldState);
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [currRoomId]);
+
+  console.log("[MultiAquarium] currRoomId", currRoomId, rooms);
+  return (
+    <div className={styles.body}>
+      {/* <img src={background} alt="배경화면"></img> */}
+      <section className={styles.SecHead}>
+        <Header rooms={roomIds} roomId={currRoomId} setRoomId={setCurrRoomId} />
+      </section>
+      <section className={styles.SecFieldCtrl}>
+        <FieldCtrl room={rooms.current && rooms.current[currRoomId]} />
+      </section>
+      <section className={styles.SecField}>
+        <MultiField room={rooms.current && rooms.current[currRoomId]} />
+      </section>
+    </div>
+  );
+}
