302b30965f8316de8597506a4e5093d800cd99d8
shinhojung814 <shinhojung814@gmail.com>
2021-11-15 04:44:27 +0900

    project-alien

--- project-alien/client/src/modals/SignInModal.js
+++ project-alien/client/src/modals/SignInModal.js
@@ -1,55 +1,164 @@
-import React, { useState } from 'react';
-import { Button, Container, Form, Modal } from 'react-bootstrap';
-// import { GoogleLogin } from 'react-google-login';
-import HorizonLine from '../components/HorizonLine'
+import React, { useState, useEffect } from "react";
+import { Button, Container, Form, Modal } from "react-bootstrap";
+import styles from "./SignInModal.module.css";
+import HorizonLine from "../components/HorizonLine";
+import * as api from "../apis/index.js";
 
-const SignInModal = ({ show, onHide, setLoginStatus, setSignInModalOn }) => {
-    // console.log("SignInModal", setLoginStatus)
+const SignInModal = ({ show, onHide, setSignInModalOn, setLoginStatus }) => {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
 
-    return (
-        <Modal
-            show={show}
-            onHide={onHide}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+  const [signInClicked, setSignInClicked] = useState(false);
+  const [signInError, setSignInError] = useState(null);
+  const [signInMessage, setSignInMessage] = useState(null);
+
+  const [signInStatus, setSignInStatus] = useState(false);
+
+  function validateSignIn(userEmail, userPassword) {
+    if (
+      (userEmail != "") &
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail)
+    ) {
+      setSignInMessage("입력하신 이메일 주소가 유효하지 않습니다.");
+      return false;
+    }
+
+    if (userPassword != "") {
+      setSignInMessage("입력하신 이메일과 패스워드가 일치하지 않습니다.");
+      return false;
+    }
+
+    if (userEmail == "" || userPassword == "") {
+      setSignInMessage("입력하지 않은 회원정보가 있습니다.");
+      return false;
+    }
+
+    setSignInStatus(true);
+    console.log("signInStatus", signInStatus);
+
+    setSignInMessage(null);
+    return true;
+  }
+
+  const postSignIn = async () => {
+    let signInData = { email: userEmail, pwd: userPassword };
+    const response = await api.post("/user/login_process", signInData);
+    console.log("response", response);
+    // const data = await response.json();
+    // console.log("data", data);
+  };
+
+  console.log("signInClicked", signInClicked);
+
+  useEffect(() => {
+    try {
+      setSignInError(null);
+      postSignIn();
+      // 회원정보 DB에서 관리하는 데이터가 signInData와 일치하면 signInSuccess() 그렇지 않으면 signInFailure() 실행
+      // ex) {signInResponse == "success" ? signInSuccess() : signInFailure()}
+      console.log("signInStatus", signInStatus);
+      if (signInStatus == true) {
+        signInSuccess();
+      } else {
+        signInFailure();
+      }
+    } catch (err) {
+      setSignInError(err);
+      console.log("SIGN-IN ERROR", signInError);
+    }
+  }, [signInClicked]);
+
+  function signInSuccess() {
+    return () => {
+      setSignInClicked(false);
+      setSignInStatus(true);
+      setLoginStatus(true);
+      console.log("signInStatus", signInStatus);
+      setSignInModalOn(false);
+    };
+  }
+
+  function signInFailure() {
+    return () => {
+      setSignInClicked(false);
+    };
+  }
+
+  const onClick = (event) => {
+    event.preventDefault();
+    setSignInMessage(null);
+    if (validateSignIn(userEmail, userPassword) == true) {
+      setSignInClicked(true);
+    }
+  };
+
+  return (
+    <Modal
+      className={styles.modal__form}
+      show={show}
+      onHide={onHide}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Container className={styles.container}>
+        <Modal.Header
+          closeButton
+          onClick={() => {
+            setUserEmail("");
+            setUserPassword("");
+            setSignInMessage(null);
+          }}
         >
-            <Container>
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        로그인
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>이메일</Form.Label>
-                            <Form.Control type="email" placeholder="helloalien@jungle.com" />
-                        </Form.Group>
+          <Modal.Title id="contained-modal-title-vcenter">로그인</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form className={styles.form__signin}>
+            <Form.Group>
+              <Form.Label>이메일</Form.Label>
+              <Form.Control
+                className={styles.form__input}
+                type="email"
+                placeholder="helloalien@jungle.com"
+                onChange={(e) => {
+                  setUserEmail(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>패스워드</Form.Label>
-                            <Form.Control type="password" placeholder="********" />
-                        </Form.Group>
+            <Form.Group>
+              <Form.Label>패스워드</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="********"
+                onChange={(e) => {
+                  setUserPassword(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
 
-                        <Button
-                            className="my-3"
-                            type="button"
-                            variant="success"
-                            style={{
-                                width: "100%"
-                            }}
-                            onClick={() => {
-                                setSignInModalOn(false);
-                                setLoginStatus(true);
-                            }}
-                        >
-                            로그인
-                        </Button>
-                    </Form>
-                </Modal.Body>
-            </Container>
-        </Modal>
-    );
+            <Form.Group className={styles.form__signin__message}>
+              {signInMessage}
+              <br />
+            </Form.Group>
+
+            <Button
+              className="my-3"
+              type="button"
+              variant="success"
+              style={{
+                width: "100%",
+              }}
+              onClick={onClick}
+            >
+              로그인
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Container>
+    </Modal>
+  );
 };
 
 export default SignInModal;
--- project-alien/client/src/modals/SignUpModal.js
+++ project-alien/client/src/modals/SignUpModal.js
@@ -1,99 +1,182 @@
-import React, { useState } from 'react';
-import { Button, Container, Form, Modal } from 'react-bootstrap';
-// import { GoogleLogin } from 'react-google-login';
-// import Axios from 'axios';
-import HorizonLine from '../components/HorizonLine.js'
+import React, { useState, useEffect } from "react";
+import { Button, Container, Form, Modal } from "react-bootstrap";
+import styles from "./SignUpModal.module.css";
+import HorizonLine from "../components/HorizonLine.js";
+import * as api from "../apis/index.js";
 
-const SignUpModal = ({ show, onHide, test }) => {
-    // const [showSignUp, setShowSignUp] = useState(true);
+const SignUpModal = ({ show, onHide, setSignUpModalOn, setLoginStatus }) => {
+  const [userNickname, setUserNickname] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userConfirm, setUserConfirm] = useState("");
 
-    // const [userName, setUserName] = userState("");
-    // const [userEmail, setUserEmail] = userState("");
-    // const [userPassword, setUserPassword] = userState("");
-    // const [userConfirm, setUserConfirm] = userState("");
+  const [signUpClicked, setSignUpClicked] = useState(false);
+  const [signUpError, setSignUpError] = useState(null);
+  const [signUpMessage, setSignUpMessage] = useState(null);
 
-    // const signup = () => {
-    //     Axios.post("http://localhost:3000/signup", {
-    //         username: userName,
-    //         password: password,
-    //     }).then(response) => {
-    //         console.log(response);
-    //     });
-    // };
+  function validateSignUp(userNickname, userEmail, userPassword, userConfirm) {
+    if (
+      (userEmail != "") &
+      !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(userEmail)
+    ) {
+      setSignUpMessage("입력하신 이메일 주소가 유효하지 않습니다.");
+      return false;
+    }
 
-    return (
-        <Modal
-            show={show}
-            onHide={onHide}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
+    if (userPassword != userConfirm) {
+      setSignUpMessage("입력하신 패스워드가 일치하지 않습니다.");
+      return false;
+    }
+
+    if (
+      userNickname == "" ||
+      userEmail == "" ||
+      userPassword == "" ||
+      userConfirm == ""
+    ) {
+      setSignUpMessage("입력하지 않은 회원정보가 있습니다.");
+      return false;
+    }
+
+    setSignUpMessage(null);
+    return true;
+  }
+
+  const postSignUp = async () => {
+    let signUpData = { userNickname, userEmail, userPassword, userConfirm };
+    const response = await api.post("/login_process", signUpData);
+    console.log("response", response);
+    // const data = await response.json();
+    // console.log("data", data);
+  };
+
+  console.log("signUpClicked", signUpClicked);
+
+  // 회원가입 버튼을 처음 눌렀을 때만 포스트 요청이 보내지도록 설정 (useEffect, signUpClicked 이용)
+  useEffect(() => {
+    try {
+      setSignUpError(null);
+      postSignUp();
+    } catch (err) {
+      setSignUpError(err);
+      console.log("SIGN-UP ERROR", signUpError);
+    }
+  }, [signUpClicked]);
+
+  function signUpSuccess() {
+    return () => {
+      setSignUpClicked(false);
+      setSignUpModalOn(false);
+    };
+  }
+
+  function signUpFailure() {
+    return () => {
+      setSignUpClicked(false);
+    };
+  }
+
+  const onClick = (event) => {
+    event.preventDefault();
+    setSignUpMessage(null);
+    if (
+      validateSignUp(userNickname, userEmail, userPassword, userConfirm) == true
+    ) {
+      setSignUpClicked(true);
+    }
+  };
+
+  return (
+    <Modal
+      show={show}
+      onHide={onHide}
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Container>
+        <Modal.Header
+          closeButton
+          onClick={() => {
+            setUserNickname("");
+            setUserEmail("");
+            setUserPassword("");
+            setUserConfirm("");
+            setSignUpMessage(null);
+          }}
         >
-            <Container>
-                <Modal.Header closeButton>
-                    <Modal.Title id="contained-modal-title-vcenter">
-                        회원가입
-                    </Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group>
-                            <Form.Label>이름</Form.Label>
-                            <Form.Control
-                                type="text"
-                                placeholder="이재열"
-                            // onChange={(e) => {
-                            //     setUserName(e.target.value);
-                            // }}
-                            />
-                        </Form.Group>
+          <Modal.Title id="contained-modal-title-vcenter">회원가입</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>닉네임</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="이재열"
+                onChange={(e) => {
+                  setUserNickname(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>이메일</Form.Label>
-                            <Form.Control
-                                type="email"
-                                placeholder="helloalien@jungle.com"
-                            // onChange={(e) => {
-                            //     setUserEmail(e.target.value);
-                            // }}
-                            />
-                        </Form.Group>
+            <Form.Group>
+              <Form.Label>이메일</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="helloalien@jungle.com"
+                onChange={(e) => {
+                  setUserEmail(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>패스워드</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="********"
-                            // onChange={(e) => {
-                            //     setUserPassword(e.target.value);
-                            // }}
-                            />
-                        </Form.Group>
+            <Form.Group>
+              <Form.Label>패스워드</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="********"
+                onChange={(e) => {
+                  setUserPassword(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
 
-                        <Form.Group>
-                            <Form.Label>패스워드 확인</Form.Label>
-                            <Form.Control
-                                type="password"
-                                placeholder="********"
-                            // onChange={(e) => {
-                            //     setUserConfirm(e.target.value);
-                            // }}
-                            />
-                        </Form.Group>
-                        <Button
-                            className="my-3"
-                            type="button"
-                            variant="success"
-                            style={{
-                                width: "100%"
-                            }}
-                        >
-                            회원가입
-                        </Button>
-                    </Form>
-                </Modal.Body>
-            </Container>
-        </Modal>
-    );
+            <Form.Group>
+              <Form.Label>패스워드 확인</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="********"
+                onChange={(e) => {
+                  setUserConfirm(e.target.value);
+                }}
+              />
+              <br />
+            </Form.Group>
+
+            <Form.Group className={styles.form__signup__message}>
+              {signUpMessage}
+              <br />
+            </Form.Group>
+
+            <Button
+              className="my-3"
+              type="button"
+              variant="success"
+              style={{
+                width: "100%",
+              }}
+              onClick={onClick}
+            >
+              회원가입
+            </Button>
+          </Form>
+        </Modal.Body>
+      </Container>
+    </Modal>
+  );
 };
 
 export default SignUpModal;
