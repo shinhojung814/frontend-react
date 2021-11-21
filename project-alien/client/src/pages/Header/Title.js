f0030546c39ae43d35452d9b10b3226eb41b7139
shinhojung814 <shinhojung814@gmail.com>
2021-11-22 04:11:51 +0900

    project-alien

--- project-alien/client/src/pages/Header/Title.js
+++ project-alien/client/src/pages/Header/Title.js
@@ -11,12 +11,21 @@ export default function Title(props) {
   const { user } = useSelector(({ user }) => ({ user: user.user }));
   //   console.log(123, user);
   // url 확인하기
-  const userMatch = useMatch("/user/:userId");
-  const challengeMatch = useMatch("/challenge/:challengeId");
-  const alienMatch = useMatch("/alien/:challengeId/:userId");
+  const userMatch = useMatch("/user/:userId/room");
+  const challengeMatch = useMatch("/challenge/:challengeId/room");
+  const newchalMatch = useMatch("/challenge/new");
+  const alienMatch = useMatch("/challenge/:challengeId/join");
   const approvalMatch = useMatch("/approval");
   const mainMatch = useMatch("/");
-  //   console.log(userMatch, challengeMatch, alienMatch, approvalMatch, mainMatch);
+
+  // console.log(
+  //   userMatch,
+  //   challengeMatch,
+  //   newchalMatch,
+  //   alienMatch,
+  //   approvalMatch,
+  //   mainMatch
+  // );
   if (!!userMatch) {
     let { params } = userMatch;
     return <div className={styles.title}>{`User-${params.userId}`}</div>;
@@ -26,25 +35,26 @@ export default function Title(props) {
     // 본 챌린지에 참가중인지 확인
     let participating;
     if (user && user.challenges) {
-      participating = user.challenges.findIndex(
-        (e) => e.Challenge_id === params.challengeId
-      );
+      participating =
+        user.challenges.findIndex(
+          (e) => e.Challenge_id === Number(challengeId)
+        ) !== -1;
     }
     return (
       <React.Fragment>
         <div className={styles.title}>{`Challenge-${params.challengeId}`}</div>
         {!!user && participating && (
-          <div className={cx("chalBtn", "chalBtn--ing")}>참가중</div>
+          <div className={cx("btn", "btn--ing")}>참가중</div>
         )}
         {!!user && !participating && (
-          <Link to={`/alien/${challengeId}/${user.id}`}>
-            <button className={cx("chalBtn", "chalBtn--start")}>
-              시작하기
-            </button>
+          <Link to={`/challenge/${challengeId}/join`}>
+            <button className={cx("btn", "btn--start")}>시작하기</button>
           </Link>
         )}
       </React.Fragment>
     );
+  } else if (!!newchalMatch) {
+    return <div className={styles.title}>{`New Challenge`}</div>;
   } else if (!!alienMatch) {
     let { params } = alienMatch;
     return (
